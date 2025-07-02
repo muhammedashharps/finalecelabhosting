0b593e8fa282e00095586ab45916b48afb527a54
ashmhdps <contactashharps@gmail.com>
2025-07-02 21:30:32 +0530

    new

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ with col2:
 st.download_button(label="Click Here To Download",
                    data=open("labappv2.apk", "rb").read(),
                    # Change "labcompanion.apk" to the name of your APK file
-                   file_name="studentcompanion.apk",
+                   file_name="labappv2.apk",
                    mime="application/vnd.android.package-archive", )
 
 # Additional information
